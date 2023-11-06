2847facda4d0a5f7e99321ffab260b0fa198f442
Sergio Conde <sergio.conde@estudiants.urv.cat>
2023-11-06 22:46:26 +0100

    Starting Back Propagation implementation

---
+++ BackPropagation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using BackPropagation;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("BackPropagation.Program", LogLevel.Debug)
+        .AddConsole();
+});
+var logger = loggerFactory.CreateLogger<Program>();
+var parametersFile = args[0];
+
+if (string.IsNullOrWhiteSpace(parametersFile))
+{
+    logger.LogError("Parameter files is mandatory!");
+    return;
+}
+
+var json = await File.ReadAllTextAsync(parametersFile);
+var trainingParameters = JsonConvert.DeserializeObject<TrainingParameters>(json);
---
+++ BackPropagation/TrainingParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace BackPropagation;
+
+public sealed record TrainingParameters
+{
+    public required string DataFile { get; init; }
+    public required int TrainingDataPercentage { get; init; }
+    //public required string ActivationFunctionType { get; init; }
+    public required int Layers { get; init; }
+    public required int UnitPerLayer { get; init; }
+    public required int Epochs { get; init; }
+    public required float LearningRate { get; init; }
+    public required float Momentum { get; init; }
+    public required string ScalingMethod { get; init; }
+    public string? ScalingMethodParams { get; init; }
+    public string? ActivationFunction { get; init; }
+    public string? OutputFile { get; init; }
+}
